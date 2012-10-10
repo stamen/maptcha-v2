917acce0dfc0448dc10fae6275c0ab5cdfb5e0ef
Michal Migurski <mike@stamen.com>
2012-10-09 22:06:51 -0700

    Connecting to us-west-1 region to work around elevated AWS error rates

--- uploader/util.py
+++ uploader/util.py
@@ -1,9 +1,11 @@
+from boto.sdb import regions
 from boto import connect_sdb, connect_sqs
 
 def connect_domain(key, secret, name):
     ''' Return a connection to a simpledb domain for atlases.
     '''
-    sdb = connect_sdb(key, secret)
+    reg = [reg for reg in regions() if reg.name == 'us-west-1'][0]
+    sdb = connect_sdb(key, secret, region=reg)
     domain = sdb.get_domain(name)
     
     return domain
