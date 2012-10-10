bf5241b78a5e119c96a27e174223d64d53190fbc
Michal Migurski <migurski@studio.(none)>
2012-10-09 20:51:45 -0700

    Really populating actual maps and atlases in simpleDB with sample data

--- uploader/app.py
+++ uploader/app.py
@@ -1,26 +1,55 @@
 from os import environ
-from boto import connect_sdb
+from uuid import uuid1
+from urllib import urlopen
+from csv import DictReader
+
+from boto import connect_sdb, connect_sqs
 from boto.exception import SDBResponseError
 from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-def connect_maps_domain(key, secret, prefix):
-    ''' Return a connection to a simpledb domain for maps.
+def connect_atlases_domain(key, secret, prefix):
+    ''' Return a connection to a simpledb domain for atlases.
     '''
     sdb = connect_sdb(key, secret)
     
     try:
-        dom = sdb.get_domain(prefix+'maps')
+        domain = sdb.get_domain(prefix+'atlases')
     except SDBResponseError:
-        dom = sdb.create_domain(prefix+'maps')
+        domain = sdb.create_domain(prefix+'atlases')
     
-    return dom
+    return domain
 
-def post_map(domain, url):
+def connect_queue(key, secret, prefix):
     '''
     '''
-    print domain, url
+    sqs = connect_sqs(key, secret)
+    queue = sqs.get_queue(prefix+'jobs')
+    
+    return queue
+
+def post_atlas(domain, queue, url):
+    '''
+    '''
+    row = DictReader(urlopen(url)).next()
+    
+    if 'address' not in row:
+        raise ValueError('Missing "address" in %(url)s' % locals())
+    
+    #
+    # Add an entry for the atlas to the atlases table.
+    #
+    atlas = domain.new_item(str(uuid1()))
+    atlas['href'] = url
+    atlas['progress'] = 0
+    atlas.save()
+    
+    #
+    # Queue the atlas for processing.
+    #
+    message = queue.new_message('create atlas %s' % atlas.name)
+    queue.write(message)
 
 @app.route('/')
 def index():
@@ -28,12 +57,15 @@ def index():
     '''
     return render_template('index.html')
 
-@app.route('/map', methods=['POST'])
-def map():
+@app.route('/atlas', methods=['POST'])
+def atlas():
     '''
     '''
-    dom = connect_maps_domain(environ['key'], environ['secret'], environ['prefix'])
-    post_map(dom, request.form['url'])
+    key, secret, prefix = environ['key'], environ['secret'], environ['prefix']
+    dom = connect_atlases_domain(key, secret, prefix)
+    queue = connect_queue(key, secret, prefix)
+
+    post_atlas(dom, queue, request.form['url'])
     
     return 'buuh'
 
