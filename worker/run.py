373ff29e6b527b24da2279e6963d0a47d18f09cc
Michal Migurski <mike@stamen.com>
2012-10-23 11:25:54 -0700

    Added 55 second loop to worker process

--- worker/run.py
+++ worker/run.py
@@ -1,4 +1,5 @@
 from os import environ
+from time import time, sleep
 import logging    
 
 from os.path import realpath, dirname, join, exists 
@@ -11,25 +12,31 @@ key, secret, prefix = get_config_vars(dirname(__file__))
 
 if __name__ == '__main__':
 
+    due = time() + 55
+    
     logging.basicConfig(level=logging.INFO)
 
     bucket = connect_bucket(key, secret, prefix+'stuff')
     queue = connect_queue(key, secret, prefix+'jobs')
     
-    try:
-        message = queue.get_messages(visibility_timeout=5)[0]
-
-    except IndexError:
-        pass
-
-    else:
-        msg = message.get_body()
-        logging.info(msg)
+    while time() < due:
+        try:
+            message = queue.get_messages(visibility_timeout=5)[0]
+    
+        except IndexError:
+            pass
+    
+        else:
+            msg = message.get_body()
+            logging.info(msg)
+            
+            if msg.startswith('populate atlas '):
+                map_dom = connect_domain(key, secret, prefix+'maps')
+                atlas_dom = connect_domain(key, secret, prefix+'atlases')
+    
+                populate_atlas(atlas_dom, map_dom, bucket, msg[len('populate atlas '):])
+            
+            queue.delete_message(message)
         
-        if msg.startswith('populate atlas '):
-            map_dom = connect_domain(key, secret, prefix+'maps')
-            atlas_dom = connect_domain(key, secret, prefix+'atlases')
-
-            populate_atlas(atlas_dom, map_dom, bucket, msg[len('populate atlas '):])
-        
-        queue.delete_message(message)
+        logging.debug('worker sleeping')
+        sleep(5)
